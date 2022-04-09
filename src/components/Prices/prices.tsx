5f0f39f5b628ed6a19ad93fa6528bead6d84e5dc
Charles-Antoine Alba <poro@wecode-it.ovh>
2022-04-09 16:59:16 +0200

    :memo: Texts update
    Updates texts to match a better SEO & understanding

--- src/components/Prices/prices.tsx
+++ src/components/Prices/prices.tsx
@@ -5,7 +5,10 @@ const Prices: React.FC = () => {
     return (<div className={classes.container}>
         <div>
             <h2>Nos forfaits</h2>
-            <h3>Prenez soin de vos routes et chantiers avec RBS !</h3>
+            <h3>RBS s’adapte à vos demandes ! </h3>
+            <p>Choisissez le forfait adapté à vos besoins ou demandez-nous conseil pour trouver le service le plus approprié selon nos différentes options (avec ou sans haute pression, balai de désherbage, karcher à main). Nous sommes disponibles 7j/7 24h/24.
+                <br /><br />Nos différentes solutions :
+            </p>
             <ul>
                 <li>
                     <h5>2 heures</h5>
--- src/components/Services/services.tsx
+++ src/components/Services/services.tsx
@@ -6,11 +6,18 @@ const Services: React.FC = () => {
         <div>
             <h2>Nos services</h2>
             <h3>Prenez soin de vos routes et chantiers avec RBS !</h3>
-            <p>Nous sommes spécialisés dans le balayage, nettoyage et le lavage des routes, chantiers, autoroutes, villes, villages, allées, parking...
-                <br />Le balayage se fait avec ou sans haute pression selon les demandes.
-                <br />Il est aussi possible d'utiliser un karcher à main pour des besoins plus spécifiques ou encore un balai de désherbage.
-                <br /><br />
-                Nous mettons tout en place pour vous satisfaire dans les meilleurs délais et conditions.
+            <p>Nous sommes spécialisés dans le balayage et nettoyage de voirie.<br />
+                Nous proposons notre matériel de haute qualité pour le nettoyage et le lavage de routes, chantiers,
+                autoroutes, villes, villages, allées, parking, sites industriels, carrières... <br /><br />
+                Le balayage vous sera proposé avec ou sans haute pression selon les besoins.<br />
+                Le chauffeur pourra utiliser un karcher à main pour des demandes plus précises ou des petites
+                surfaces.<br />
+                La balayeuse pourra également être équipée d’un balai de désherbage, afin d’arracher les mauvaises
+                herbes situées dans le fil d’eau.<br /><br />
+                Nous mettons tout en place pour vous satisfaire dans les meilleurs délais et conditions.<br /><br />
+                Acteur dans le domaine de la propreté nous accompagnons nos clients professionnels et particuliers.<br /><br />
+                Vous êtes une entreprise du bâtiment et travaux publics, collectivités, sites industriels, particuliers…<br /><br />
+                Nous avons le service de nettoyage adapté à votre besoin.
             </p>
         </div>
     </div>)
